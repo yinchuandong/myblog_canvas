c8e0d96f9e29d1d2aaa095ecc4592ec87e75fee8
yinchuandong <yincd520@sina.com>
2015-02-14 10:36:49 +0800

    fisrt commit

---
+++ js/resource.js
@@ -0,0 +1,55 @@
+/**
+ * Created by wangjiewen on 14-11-2.
+ */
+
+var resource = {
+    about_future_box: 'img/about-future-box.png',
+    about_me_box: 'img/about-future-box.png',
+    bg_cloud_1: 'img/bg/cloud-1.png',
+    bg_cloud_2: 'img/bg/cloud-2.png',
+    bg_cloud_3: 'img/bg/cloud-3.png',
+    bg_cloud_4: 'img/bg/cloud-4.png',
+    bg_cloud_5: 'img/bg/cloud-5.png',
+    bg_star_1: 'img/bg/star-1.png',
+    bg_star_2: 'img/bg/star-2.png',
+    bg_star_3: 'img/bg/star-3.png',
+    bg_star_4: 'img/bg/star-4.png',
+    bg_star_5: 'img/bg/star-5.png',
+    boom_1: 'img/boom/trophy-boom-1.png',
+    boom_2: 'img/boom/trophy-boom-2.png',
+    boom_3: 'img/boom/trophy-boom-3.png',
+    comp: 'img/comp-1.gif',
+    contact_box: 'img/contact-box.png',
+    earth: 'img/contact-box.png',
+    rocket_1: 'img/rocket-1.png',
+    star_1: 'img/star-1.png',
+    star_2: 'img/star-2.png',
+    trophy_box: 'img/trophy-box.png',
+    trophy_rock_1: 'img/trophy-rock-1.png',
+    trophy_rock_2: 'img/trophy-rock-2.png',
+    trophy_rock_small_1: 'img/trophy-rock-small-1.png',
+    work_1: 'img/work-1.png',
+    work_2: 'img/work-2.png',
+    work_3: 'img/work-3.png',
+    work_4: 'img/work-4.png',
+    work_5: 'img/work-5.png',
+    work_6: 'img/work-6.png',
+    work_7: 'img/work-7.png',
+    work_8: 'img/work-8.png',
+    work_box: 'img/work-box.png'
+
+};
+
+var g_resources = [
+    resource.bg_dirt,
+    resource.grass_lower,
+    resource.grass_upper,
+    resource.mole_1,
+    resource.mole_laugh1,
+    resource.mole_laugh2,
+    resource.mole_laugh3,
+    resource.mole_thump1,
+    resource.mole_thump2,
+    resource.mole_thump3,
+    resource.mole_thump4
+];
